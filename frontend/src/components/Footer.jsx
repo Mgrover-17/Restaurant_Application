9039b7b4733e3ce5dcd065983bfe67d0738965e0
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2024-12-07 17:03:13 +0530

    done with frontend part

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Chef&apos;s Table</div>
+          <div className="right">
+            <p>Rajpura, Punjab</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Manvi Grover</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Manvi Grover.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
